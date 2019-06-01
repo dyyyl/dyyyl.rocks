073d6af4354ee471178f8be2c47e2ab3c46bc4cb
dyyyl <hey@dyyyl.rocks>
2019-06-01 20:00:12 +0200

    we're just trying things out here

--- src/shared/hooks/useWindowWidth.js
+++ src/shared/hooks/useWindowWidth.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 
 const useWindowWidth = () => {
-  const [width, setWidth] = useState(window.innerWidth);
+  const [width, setWidth] = useState(window ? window.innerWidth : undefined);
 
   useEffect(() => {
     const handleResize = () => setWidth(window.innerWidth);
