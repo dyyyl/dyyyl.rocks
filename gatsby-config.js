d0886a7f8fdda2f4940fcacb7c24136680070c87
dyyyl <hey@dyyyl.rocks>
2019-06-03 15:55:19 +0200

    The world deserves this

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,7 +66,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/shared/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/shared/images/myFace.jpg', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
