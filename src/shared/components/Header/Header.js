04fb313f0ec6c15114ee70ec14c81bff9a1b9aa3
dyyyl <hey@dyyyl.rocks>
2019-06-29 16:15:25 +0200

    add time

--- src/shared/components/Header/Header.js
+++ src/shared/components/Header/Header.js
@@ -1,22 +1,38 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import getToday from '../../helpers/getToday';
 
 import HeaderContainer from './HeaderContainer';
 import Link from '../Link';
 import TitleContainer from './TitleContainer';
 
-const Header = ({ siteTitle }) => (
-  <>
-    <TitleContainer>
-      <h1>
-        <Link to="/" data-testid="title">
-          {siteTitle}
-        </Link>
-      </h1>
-    </TitleContainer>
-    <HeaderContainer />
-  </>
-);
+const Header = ({ siteTitle }) => {
+  const [today, setToday] = useState(getToday());
+
+  useEffect(() => {
+    setToday(getToday());
+  }, []);
+
+  return (
+    <>
+      <TitleContainer>
+        <h1>
+          <Link to="/" data-testid="title">
+            {siteTitle}
+          </Link>
+        </h1>
+      </TitleContainer>
+      <HeaderContainer>
+        <span style={{ fontFamily: 'Courier New', marginRight: '5rem' }}>
+          <b>
+            <time dateTime={new Date()}>{today}</time> Berlin
+          </b>
+        </span>
+      </HeaderContainer>
+    </>
+  );
+};
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
--- src/shared/components/Header/HeaderContainer.js
+++ src/shared/components/Header/HeaderContainer.js
@@ -3,6 +3,9 @@ import styled from 'styled-components';
 const HeaderContainer = styled.header`
   height: 100%;
   border-bottom: 1px solid black;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
 
   grid-area: header;
 `;
